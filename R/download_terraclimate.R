f7ccd55929089f9badafbf31476fdde213bcc7a1
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-05-31 14:20:21 +0200

    adding switcher to download_terraClimate function to include e, tmin, tmax, and tavg variables

--- R/download_terraclimate.R
+++ R/download_terraclimate.R
@@ -1,6 +1,6 @@
 #' TerraClimate data downloader
 #'
-#' Function for downloading TerraClimate evapotranspiration data.
+#' Function for downloading TerraClimate data.
 #'
 #' @importFrom utils download.file
 #' @param folder_path a character string with the path where the data will be downloaded.
@@ -16,22 +16,48 @@
 #' \item{"monthly",}
 #' \item{"yearly".}
 #' }
+#' @param variable a character string indicating the variable to download. Suitable options are:
+#' \itemize{
+#' \item{"e" for evapotranspiration,}
+#' \item{"tmin" for minimum temperature,}
+#' \item{"tmax" for maximum temperature,}
+#' \item{"tavg" for average temperature.}
+#' }
 #' @return No return value, called to download the data set.
 #' @keywords internal
 #' @export
-download_terraclimate <- function(folder_path = ".", domain = "raw", time_res = "monthly"){
+download_terraclimate <- function(folder_path = ".", domain = "raw", time_res = "monthly", variable = "e") {
   old_options <- options()
   options(timeout = 6000)
   on.exit(options(old_options))
-  if (domain == "raw" | domain == "land"){
+  
+  if (domain == "raw" | domain == "land") {
     domain <- "land"
   } else {
     warning(paste0('The ', domain, ' domain is not available'))
   }
+  
   zenodo_base <- "https://zenodo.org/record/7990413/files/"
   zenodo_end <- "?download=1"
-  file_name <- paste0("terraclimate_e_mm_", domain, "_195801_202112_025_", time_res, ".nc")
+  
+  switch(variable,
+         "e" = {
+           file_name <- paste0("terraclimate_e_mm_", domain, "_195801_202112_025_", time_res, ".nc")
+         },
+         "tmin" = {
+           file_name <- paste0("terraclimate_tmin_", domain, "_19580101_20221231_025_", time_res, ".nc")
+         },
+         "tmax" = {
+           file_name <- paste0("terraclimate_tmax_", domain, "_19580101_20221231_025_", time_res, ".nc")
+         },
+         "tavg" = {
+           file_name <- paste0("terraclimate_tavg_", domain, "_19580101_20221231_025_", time_res, ".nc")
+         },
+         stop("Invalid variable option provided.")
+  )
+  
   file_url <- paste0(zenodo_base, file_name, zenodo_end)
   file_destination <- paste(folder_path, file_name, sep = "/")
+  
   try(download.file(file_url, file_destination, mode = "wb"), silent = TRUE)
 }
