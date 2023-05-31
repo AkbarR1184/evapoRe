8c5b11acb30702bc21f39d354cdd6341e33c30ad
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-05-31 13:35:26 +0200

    changing zenodo repo to newer version

--- R/download_era5.R
+++ R/download_era5.R
@@ -24,7 +24,7 @@ download_era5 <- function(folder_path = ".", domain = "raw", time_res = "monthly
   options(timeout = 6000)
   on.exit(options(old_options))
   if (domain == "raw"){domain <- "global"}
-  zenodo_base <- "https://zenodo.org/record/7946163/files/"
+  zenodo_base <- "https://zenodo.org/record/7990413/files/"
   zenodo_end <- "?download=1"
   file_name <- paste0("era5_e_mm_", domain, "_195901_202112_025_", time_res, ".nc")
   file_url <- paste0(zenodo_base, file_name, zenodo_end)
