e5524d638dd8b714e94bb99d1789c8ead7ce4ea6
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-06-06 13:10:15 +0200

    Change in function to avoid warnings for import

--- R/pet_aux.R
+++ R/pet_aux.R
@@ -14,9 +14,9 @@ pet_aux <- function(x){
   n_day <-c(17,47,75,105,135,162,198,228,258,288,318,344)
   mon_day <- c(31,28,31,30,31,30,31,31,30,31,30,31)
   lookup <- data.frame(mon,n_day,mon_day)
-  match_row <- match(lubridate::month(getZ(x))[1:12], lookup$mon)
+  match_row <- match(month(getZ(x))[1:12], lookup$mon)
   dist_ratio <- (1.0 + 0.0330 * cos(2*pi *lookup$n_day[match_row] / 365.25))*lookup$mon_day[match_row]
-  delta <- 0.409 * sin( 2*pi * lubridate::yday(getZ(x)) / 365.25 - 1.39)
+  delta <- 0.409 * sin( 2*pi * yday(getZ(x)) / 365.25 - 1.39)
   delta <- delta[1:12]
   latitude <- raster::setValues(x[[1:12]], yFromCell(x[[1:12]], 1:ncell(x[[1:12]]))*pi/180)
   omega <- calculate_omega(latitude,delta)
