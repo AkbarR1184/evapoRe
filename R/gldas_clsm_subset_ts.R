07828218864153a1ed4f98f5733360e38b87ce32
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2023-06-15 16:39:21 +0200

    Adding datasets documentation as R file

---
+++ R/gldas_clsm_subset_ts.R
@@ -0,0 +1,13 @@
+#' Monthly Evapotranspiration data
+#'
+#' A subset of GLDAS monthly Evapotranspiration data in mm over 2-28E, 42-58N. More details of the raw data can be found \href{https://ldas.gsfc.nasa.gov/gldas}{here}.
+#'
+#' @format A data.table with 120 obs. of 4 variables:
+#' \describe{
+#'   \item{date}{IDate format \%Y-\%m-\%d}
+#'   \item{value}{monthly average values}
+#'   \item{name}{full name of the data set}
+#'   \item{type}{source type of the data set}
+#' }
+#' @source National Aeronautics and Space Administration (NASA)
+"gldas_clsm_subset_ts"
---
+++ R/pet_oudin_global_ts.R
@@ -0,0 +1,14 @@
+#' Monthly Potential Evapotranspiration data
+#'
+#' Monthly Potential Evapotranspiration data in mm calculated by Oudin method. More details of the used method can be found \href{https://www.sciencedirect.com/science/article/pii/S0022169404004056}{here}.
+#'
+#' @format A data.table with 120 obs. of 4 variables:
+#' \describe{
+#'   \item{date}{IDate format \%Y-\%m-\%d}
+#'   \item{value}{monthly average values}
+#'   \item{name}{full name of the data set}
+#'   \item{type}{source type of the data set}
+#' }
+#' @source Data was calculated using the Oudin method based on raw temperature data. More details of the raw data
+#'  can be found \href{https://www.climatologylab.org/terraclimate.html}{here}.
+"pet_oudin_global_ts"
