3245f6f8d9eb772317d528bbc60e9554f96d63b5
unknown <rahmati_ziveh@fzp.czu.cz>
2023-11-25 21:42:47 +0100

    add test file

---
+++ tests/testthat/test-evapoRe.R
@@ -0,0 +1,12 @@
+test_that("evapoRe works", {
+  dates <- seq(as.Date("2023-01-01"), by = "month", length.out = 12)
+  values <- array(1:120, dim = c(10, 10, 12))
+  x <- raster::brick(values)
+  x <- raster::setZ(x, dates)
+  original_dims <- dim(x)
+  result <- muldpm(x)
+  expect_type(result, "S4")
+  expect_equal(original_dims, dim(result),
+               info = "Dimensions should remain the same before and after applying muldpm")
+})
+
