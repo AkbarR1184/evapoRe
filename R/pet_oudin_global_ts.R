46b921ca61eb1654468d8fdb550e88f396884b24
unknown <rahmati_ziveh@fzp.czu.cz>
2023-11-26 14:46:32 +0100

    add wordlist

--- R/pet_oudin_global_ts.R
+++ R/pet_oudin_global_ts.R
@@ -1,6 +1,6 @@
 #' Monthly Potential Evapotranspiration data
 #'
-#' Monthly Potential Evapotranspiration data in mm calculated by Oudin method. More details of the used method can be found \samp{https://www.sciencedirect.com/science/article/pii/S0022169404004056}{here}.
+#' Monthly Potential Evapotranspiration data in mm calculated by Oudin method. More details of the used method can be found \samp{https://www.sciencedirect.com/science/article/pii/S0022169404004056}.
 #'
 #' @format A data.table with 120 obs. of 2 variables:
 #' \describe{
@@ -8,5 +8,5 @@
 #'   \item{value}{monthly average values}
 #' }
 #' @source Data was calculated using the Oudin method based on raw temperature data. More details of the raw data
-#'  can be found \samp{https://journals.ametsoc.org/view/journals/bams/103/3/BAMS-D-21-0145.1.xml}{here}.
+#'  can be found \samp{https://journals.ametsoc.org/view/journals/bams/103/3/BAMS-D-21-0145.1.xml}.
 "pet_oudin_global_ts"
