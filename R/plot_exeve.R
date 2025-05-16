6f95677be1235fb9909460d5269b153fed740b31
AkbarR1184 <rahmati_ziveh@fzp.czu.cz>
2025-05-16 17:57:46 +0200

    -minor changes in plot_exeve() function

--- R/plot_exeve.R
+++ R/plot_exeve.R
@@ -10,27 +10,22 @@
 #' - `"above_low_thres_id"`: Identifier for values above a lower threshold  
 #' - `"event_id"`: Identifier for broader event periods (can be `NA`)  
 #'
-#' `my_palette` is a character vector of two colors specifying the color for  
-#' extreme events and broader events, respectively.
-#'
-#' `event_color` defines the color for marking event periods along the x-axis.
-#'
-#' `shapes` is a numeric vector of three shape codes for:  
+#' `exeve_pal` is a character vector of three colors used to style:
 #' (1) extreme points, (2) event points, and (3) event markers on the x-axis.
+#'
+#' `shapes` is a numeric vector of three shape codes corresponding to:
+#' (1) extreme points, (2) event points, and (3) event markers.
 #'
 #' @rawNamespace import(data.table, except = c("month", "yday", "year"))
 #' @import ggplot2
 #' @param x A data.table (see details)
-#' @param my_palette Character vector of 2 colors: (1) extreme, (2) event
-#' @param event_color Character; color for x-axis event markers (default = "#a9cce0")
+#' @param exeve_pal Character vector of 3 colors: (1) extreme, (2) event, (3) marker
 #' @param title Optional character string; plot title
 #' @param shapes Numeric vector of 3 shape codes (see details)
 #' @return A ggplot object
 #' @export
 #' @examples
 #' \donttest{
-#'
-#' # Create example data
 #' dt <- data.table(
 #'   date = seq(as.Date("2020-01-01"), as.Date("2020-03-01"), by = "day"),
 #'   value = runif(61, 0, 5),
@@ -39,17 +34,16 @@
 #'   event_id = c(rep(NA, 25), rep(1, 10), rep(NA, 26))
 #' )
 #'
-#' plot_exeves(dt)
+#' plot_exeve(dt, exeve_pal = c("#4D648D", "#337BAE", "#a9cce0"))
 #' }
-plot_exeves <- function(
+plot_exeve <- function(
     x,
-    my_palette = c("#4D648D", "#337BAE"),
-    event_color = "#a9cce0",
+    exeve_pal = c("#4D648D", "#337BAE", "#a9cce0"),
     title = NULL,
     shapes = c(0, 16, 15)
 ) {
   stopifnot("data.table" %in% class(x))
-  if (length(my_palette) != 2) stop("my_palette must have 2 colors.")
+  if (length(exeve_pal) != 3) stop("exeve_pal must have 3 colors.")
   if (length(shapes) != 3) stop("shapes must have 3 numeric values.")
   
   required_cols <- c("date", "value", "extreme_id", "above_low_thres_id", "event_id")
@@ -63,7 +57,7 @@ plot_exeves <- function(
     geom_point(
       data = x[!is.na(extreme_id)],
       aes(date, value),
-      col = my_palette[1],
+      col = exeve_pal[1],
       size = 4,
       shape = shapes[1]
     ) +
@@ -71,7 +65,7 @@ plot_exeves <- function(
     geom_point(
       data = x[!is.na(event_id)],
       aes(date, value),
-      col = my_palette[2],
+      col = exeve_pal[2],
       size = 3,
       shape = shapes[2],
       alpha = 0.5
@@ -80,7 +74,7 @@ plot_exeves <- function(
     geom_point(
       data = x[!is.na(event_id)],
       aes(date, y = 0.6),
-      col = event_color,
+      col = exeve_pal[3],
       size = 2,
       shape = shapes[3]
     ) +
